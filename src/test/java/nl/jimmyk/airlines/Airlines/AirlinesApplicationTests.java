322ff3280a0a97244335fbbaae2ff5d1c7f8fb3d
jknoot <jimmy.knoot@capgemini.com>
2018-02-01 09:46:47 +0100

    initial commit

---
+++ src/test/java/nl/jimmyk/airlines/Airlines/AirlinesApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.jimmyk.airlines.Airlines;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirlinesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
